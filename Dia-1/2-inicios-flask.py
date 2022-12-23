258037b35333224423030fb75960c8cbb66975bc
Yohmontana <estebanjcarlo@gmail.com>
2022-12-22 23:14:23 -0500

    algunos cambios

--- Dia-1/2-inicios-flask.py
+++ Dia-1/2-inicios-flask.py
@@ -1,11 +1,58 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app=app, origins=['http://127.0.0.1:5500'], methods=['GET', 'POST'])
+
+usuarios = [
+    {
+        'nombre' : 'Eduardo',
+        'apellido' : 'Juarez'
+    },
+    {
+        'nombre' : 'Juana',
+        'apellido' : 'Rodriguez'
+    },
+    {
+        'nombre' : 'Roberto',   
+        'apellido' : 'Castillo'
+    },
+]
+
 
 @app.route('/')
 def inicio():
     return "Hola desde mi servidor de Flask"
 
+@app.route('/mostrar-hora', methods=['GET','POST'])
+def mostrarHora():
+    print(request.method)
+    if request.method == 'GET':
+         return{
+        'content': 'Me hiciste GET'
+    }
+    elif request.method == 'POST':
+        return{
+        'content': 'Me hiciste POST'
+        }
 
+    return{
+        'content': '22:50:15'
+    }
 
+@app.route('/usuarios', methods={'GET', 'POST'})
+def gestionUsuario():
+    if request.method == 'GET':
+        return{
+            'message': 'Los usuarios son',
+            'content': usuarios
+        }
+    elif request.method == 'POST':
+        print(request.get_json())
+        data = request.get_json()
+        usuarios.append(data)
+        return{
+             'message': 'Usuario agregado exitosamente'
+        }
 app.run(debug=True)
