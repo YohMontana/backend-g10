63e4bbf084a8d6987b0a5f1e95bb665eb5c9297e
Yohmontana <estebanjcarlo@gmail.com>
2023-02-13 11:05:13 -0500

    semana 006 flask

--- app.py
+++ app.py
@@ -1,52 +1,7 @@
-from flask import Flask, request
-from controllers.productos_controller import ProductosController
-from flask_migrate import Migrate
-from db import db
+from flask import Flask
 
 app = Flask(__name__)
-
-
-
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///project.db"
-
-db.init_app(app)
-
-migrate = Migrate(app, db)
 
 @app.route("/")
 def index():
     return "Mi aplicacion con Flask :D"
-
-@app.route("/productos/lista", methods=['GET'])
-def productosListar():
-  controller = ProductosController()
-  return controller.listarProductos()
-
-@app.route("/productos/crear", methods=['POST'])
-def productosCrear():
-    controller = ProductosController()
-    return controller.crearProducto(request.json)
-
-@app.route("/productos/eliminar/<int:producto_id>", methods=['DELETE'])
-def productosEliminar(producto_id):
-    controller = ProductosController()
-    return controller.eliminarProducto(producto_id)
-
-@app.route("/productos/actualizar/<int:producto_id>", methods=['PUT'])
-def productosActualizar(producto_id):
-    controller = ProductosController()
-    return controller.actualizarProducto(producto_id, request.json)
-
-@app.route("/productos/buscar/<float:precio>", methods=['GET'])
-def productosBuscar(precio):
-    controller = ProductosController()
-    return controller.buscarProductos(precio)
-
-
-
-
-
-
-
-if __name__ == '__main__':
-  app.run(debug=True)
--- repaso/app.py
+++ repaso/app.py
@@ -1,29 +1,28 @@
-class Operaciones:  
+class Operaciones:
     def suma(a, b):
-      return a + b    
+        return a + b
 
     def multiplicar(a, b):
-      return a * b    
+        return a * b
 
     def restar(a, b):
-      return a - b
+        return a - b
 
 resultado = Operaciones.suma(1,1)
-resultado_2=Operaciones.multiplicar(2,2)
-
-print(resultado_2)
+resultado_2 = Operaciones.multiplicar(2,2) 
 
 class Operaciones_2:
-  def __init__(self, a, b):
-    self.primero_numero = a 
-    self.segundo_numero = b
+    def __init__(self, a, b):
+        self.primer_numero = a
+        self.segundo_numero = b 
+    
+    def restar(self):
+        return self.primer_numero - self.segundo_numero
+    
+    def suma(self):
+        return self.primer_numero + self.segundo_numero
 
-  def restar(self):
-    return self.primero_numero - self.segundo_numero
-  
-  def suma(self):
-    return self.primero_numero + self.segundo_numero
 
-resultado_3 = Operaciones_2(3, 4)
+resultado_3 = Operaciones_2(3,4)
 
-print(resultado_3.restar())
+print(resultado_3.suma())
